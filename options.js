976e968f8cccb0d877dc6b7b7409e924cdb18698
Fotis Koutoupas <fotis.koutoupas@gmail.com>
2017-11-14 05:21:02 +0200

    Add type-in setting of shortcut in addon prefs

---
+++ options.js
@@ -0,0 +1,35 @@
+const preferencesForm = document.querySelector('#preferences');
+const shortcutInput = document.querySelector('#shortcut');
+const restoreDefaultShortcutButton = document.querySelector('#restore-default-shortcut')
+
+const defaultShortcut = browser.runtime.getManifest().commands.toggle_pinned_status.suggested_key.default;
+
+browser.storage.local.get()
+  .then(
+    storage => {
+      console.log(storage)
+      shortcutInput.value = storage.shortcut || defaultShortcut;
+    },
+    console.error
+  );
+
+preferencesForm.addEventListener('submit', event => {
+  event.preventDefault();
+
+  setShortcut(shortcutInput.value)
+});
+
+restoreDefaultShortcutButton.addEventListener('click', event => {
+  event.preventDefault();
+
+  setShortcut(defaultShortcut);
+  shortcutInput.value = defaultShortcut;
+})
+
+function setShortcut(shortcut) {
+  try {
+    browser.storage.local.set({shortcut});
+  } catch(e) {
+    console.error(e)
+  }
+}
