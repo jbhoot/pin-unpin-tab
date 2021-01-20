d93b48a2d82cfb0cdf606c3ce496f507d72b9ba9
Jayesh Bhoot <jayesh@bhoot.sh>
2021-01-20 13:48:45 +0530

    make long click tracking reliable

--- long_click.ml
+++ long_click.ml
@@ -13,8 +13,6 @@ let clicked_on_element_scrollbar : Dom_html.mouseEvent Js.t -> bool = fun ev ->
   (float_of_int ev##.clientY) > bounding_rect##.top +. (float_of_int ev_target##.clientHeight)
 
 let clicked_only_left_button : Dom_html.mouseEvent Js.t -> bool = fun ev ->
-    (* ev##.button = 0 && *)
-    (* ev##.buttons = 1 && *)
     Dom_html.buttonPressed ev = Dom_html.Left_button &&
     ev##.shiftKey = Js._false &&
     ev##.ctrlKey = Js._false &&
@@ -34,56 +32,73 @@ let request_toggle after =
     after
 
 let cancel_toggle request_id =
-  let%lwt () = Lwt.pick [
-    (let%lwt _ = Lwt_js_events.mouseup Dom_html.window in
-      Lwt.return (Dom_html.clearTimeout request_id));
-    (let%lwt _ = Lwt_js_events.mousemove Dom_html.window in
-      Lwt.return (Dom_html.clearTimeout request_id))
-  ]
-  in Lwt.return ()
+  let%lwt () = 
+    Lwt.pick [
+      (let%lwt (_ : Dom_html.mouseEvent Js.t) = Lwt_js_events.mouseup Dom_html.window in
+        Lwt.return (Dom_html.clearTimeout request_id));
+      (let%lwt (_ : Dom_html.mouseEvent Js.t) = Lwt_js_events.mousemove Dom_html.window in
+        Lwt.return (Dom_html.clearTimeout request_id))]
+  in
+  Lwt.return ()
 
 let trigger_long_click_mechanism ev _ =
   if is_valid_click ev then
-    let _ = let open Promise.Syntax in
-    let* prefs = Js.Unsafe.global##.browser##.storage##.local##get Common.prefs_query in
+    let open Promise.Syntax in
+    let (_ : unit Promise.t) = 
+      let* prefs = Js.Unsafe.global##.browser##.storage##.local##get Common.prefs_query in
       let request_id = request_toggle prefs##.longClickToggleTime in
-      let _ = cancel_toggle request_id in
+      let (_ : unit Lwt.t) = cancel_toggle request_id in
       Promise.resolve ()
     in
     Lwt.return ()
   else
     Lwt.return ()
 
-let _ = 
-  let promise =
-    Lwt.bind
-      (Lwt_js_events.mousedowns Dom_html.window trigger_long_click_mechanism)
-      (fun () -> Lwt.return ())
-    (* let promise =
-      let%lwt () = Lwt_js_events.mousedowns Dom_html.window trigger_long_click_mechanism in
-      Lwt.return () *) 
+type wait =
+  | WaitUntilTimeout
+  | WaitUntilMouseUp 
+  | WaitUntilMouseMove
+
+let _ =
+  let wait_until_timeout () =
+    let%lwt _ = Lwt_js.sleep 1. in
+    Firebug.console##log (Js.string "triggered");
+    Lwt.return WaitUntilTimeout
+  in
+
+  let interrupted_by_mouse_up () =
+    let%lwt (_ : Dom_html.mouseEvent Js.t) = Lwt_js_events.mouseup Dom_html.window in
+    Firebug.console##log (Js.string "mouseuped");
+    Lwt.return WaitUntilMouseUp
+  in
+
+  let interrupted_by_mouse_move () =
+    let%lwt (_ : Dom_html.mouseEvent Js.t) = Lwt_js_events.mousemove Dom_html.window in
+    Firebug.console##log (Js.string "mousemved");
+    Lwt.return WaitUntilMouseMove
+  in
+
+  let (promise : unit Lwt.t) =
+    let%lwt () = 
+      Lwt_js_events.mousedowns 
+        Dom_html.window
+        (fun ev _ -> 
+          if is_valid_click ev then
+            let%lwt waited = Lwt.pick [wait_until_timeout (); interrupted_by_mouse_up (); interrupted_by_mouse_move ();] in
+              match waited with
+              | WaitUntilMouseUp -> Lwt.return ()
+              | WaitUntilMouseMove -> Lwt.return ()
+              | WaitUntilTimeout -> 
+                  let _ = Js.Unsafe.global##.browser##.runtime##sendMessage (Js.string "toggle") in
+                  Lwt.return ()
+          else
+            Lwt.return ()
+          )
+    in
+    Lwt.return ()
   in
     Js.Unsafe.global##.browser##.storage##.onChanged##addListener 
       (fun changes ->
-        let () = Firebug.console##log (Js.string "cnclng") in
+        Firebug.console##log (Js.string "cnclng");
         if not (Js.to_bool changes##.longClickToggle##.newValue) then
           Lwt.cancel promise)
-
-(* let rec fn () =
-  let ev = Lwt_js_events.mousedown Dom_html.window in
-    let getting_prefs = Js.Unsafe.global##.browser##.storage##.local##get Common.prefs_query in
-    let _ = getting_prefs##then_
-              ~fulfilled: (fun prefs -> 
-                if Js.to_bool prefs##.longClickToggle then
-
-              )
-    let _ = let open Promise.Syntax in
-    let* prefs = Js.Unsafe.global##.browser##.storage##.local##get Common.prefs_query in
-      if Js.to_bool prefs##.longClickToggle then
-        (* invoke *)
-        fn ()
-      else
-        ()
-      let request_id = request_toggle prefs##.longClickToggleTime in
-      let _ = cancel_toggle request_id in
-    Promise.resolve () *)
--- prefs.ml
+++ prefs.ml
@@ -7,56 +7,67 @@ let configure_long_click_toggle prefs =
   match Dom_html.getElementById_coerce "longClickToggle" Dom_html.CoerceTo.input with
     | None -> ()
     | Some checkbox ->
-        checkbox##.checked := prefs##.longClickToggle;
+        let () = checkbox##.checked := prefs##.longClickToggle in
+
         let save_config checked =
           let upd = object%js 
             val longClickToggle = checked
           end in
           ignore(storage##set upd)
         in
+
         let toggle_time_input checked =
           match Dom_html.getElementById_coerce "longClickToggleTime" Dom_html.CoerceTo.input with
           | None -> ()
           | Some textbox -> textbox##.disabled := Js.bool (not (Js.to_bool checked))
         in
-        ignore(Dom_html.addEventListener
-          checkbox
-          Dom_html.Event.change
-          (Dom_html.handler (fun ev ->
-            Js.Opt.iter
-              (Dom_html.CoerceTo.input (Dom_html.eventTarget ev))
-              (fun input ->
-                save_config input##.checked;
-                toggle_time_input input##.checked);
-            Js._true))
-          Js._true)
+
+        let (_ : unit Lwt.t) =
+          let%lwt () =
+            Lwt_js_events.changes
+              checkbox
+              (fun ev _ ->
+                Js.Opt.iter
+                  (Dom_html.CoerceTo.input (Dom_html.eventTarget ev))
+                  (fun input ->
+                    save_config input##.checked;
+                    toggle_time_input input##.checked);
+                Lwt.return ()) in
+          Lwt.return ()
+        in
+        ()
 
 let configure_long_click_time_input prefs =
   match Dom_html.getElementById_coerce "longClickToggleTime" Dom_html.CoerceTo.input with
     | None -> ()
     | Some textbox ->
-        textbox##.value := prefs##.longClickToggleTime;
-        textbox##.disabled := Js.bool (not (Js.to_bool prefs##.longClickToggle));
-        ignore(Dom_html.addEventListener
-          textbox
-          Dom_html.Event.change
-          (Dom_html.handler (fun ev -> 
-            Js.Opt.iter
-              (Dom_html.CoerceTo.input (Dom_html.eventTarget ev))
-              (fun input -> 
-                let upd = object%js 
-                  val longClickToggleTime = input##.value
-                end in
-                ignore (storage##set upd));
-            Js._true))
-          Js._true)
+        let () = textbox##.value := prefs##.longClickToggleTime in
+        let () = textbox##.disabled := Js.bool (not (Js.to_bool prefs##.longClickToggle)) in
 
+        let (_ : unit Lwt.t) =
+          let%lwt () = 
+            Lwt_js_events.changes 
+              textbox 
+              (fun ev _ -> 
+                Js.Opt.iter
+                  (Dom_html.CoerceTo.input (Dom_html.eventTarget ev))
+                  (fun input -> 
+                    let upd = object%js 
+                      val longClickToggleTime = input##.value
+                    end in
+                    ignore (storage##set upd));
+                Lwt.return ()) in
+          Lwt.return ()
+        in
+        ()
 
-let _ =
-  let%lwt _ = Lwt_js_events.domContentLoaded () in
-    let open Promise.Syntax in
-    ignore(let* curr_prefs = storage##get Common.prefs_query in
-      configure_long_click_toggle curr_prefs;
-      configure_long_click_time_input curr_prefs;
-      Promise.resolve ());
-    Lwt.return ()
+let (_ : unit Lwt.t) =
+  let%lwt () = Lwt_js_events.domContentLoaded () in
+    let (_ : unit Promise.t) =
+      let open Promise.Syntax in
+      let* curr_prefs = storage##get Common.prefs_query in
+      let () = configure_long_click_toggle curr_prefs in
+      let () = configure_long_click_time_input curr_prefs in
+      Promise.resolve ()
+    in
+  Lwt.return ()
