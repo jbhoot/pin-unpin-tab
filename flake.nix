44a5d84a4f49fea647ba661879640dfb863aee81
Jayesh Bhoot <jayesh@bhoot.sh>
2022-05-09 18:38:15 +0530

    Clean up flake

--- flake.nix
+++ flake.nix
@@ -2,35 +2,39 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/1f06456eabe9f768f87a26d3ff8b2dc14eb4046d";
-    mypkgs.url = "github:jayesh-bhoot/nix-pkgs/60287b2ad6005e79df45cfe699b162cc6fce997e";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    mypkgs.url = "github:jayesh-bhoot/nix-pkgs";
   };
 
   outputs = { self, nixpkgs, mypkgs }:
     let
-      system = "x86_64-darwin";
-      pkgs = import nixpkgs { inherit system; };
+      systems = [ "x86_64-darwin" "aarch64-darwin" "x86_64-linux" ];
+      createDevShell = system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        pkgs.mkShell {
+          buildInputs = with pkgs; [
+            ocaml
+            ocamlPackages.findlib
+            dune_2
+            ocamlPackages.ocaml-lsp
+            ocamlformat
+            ocamlPackages.ocamlformat-rpc-lib
+            ocamlPackages.utop
+
+            ocamlPackages.js_of_ocaml
+            ocamlPackages.js_of_ocaml-ppx
+            ocamlPackages.js_of_ocaml-lwt
+            ocamlPackages.lwt_ppx
+            mypkgs.packages.${system}.promise_jsoo
+
+            nodejs
+            nodePackages.web-ext
+          ];
+        };
     in
     {
-      devShell.${system} = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          ocaml
-          ocamlPackages.findlib
-          dune_2
-          ocamlPackages.ocaml-lsp
-          ocamlformat
-          ocamlPackages.utop
-          fswatch
-
-          ocamlPackages.js_of_ocaml
-          ocamlPackages.js_of_ocaml-ppx
-          ocamlPackages.js_of_ocaml-lwt
-          ocamlPackages.lwt_ppx
-          mypkgs.packages.${system}.promise_jsoo
-
-          nodejs
-          nodePackages.web-ext
-        ];
-      };
+      devShell = nixpkgs.lib.genAttrs systems createDevShell;
     };
 }
