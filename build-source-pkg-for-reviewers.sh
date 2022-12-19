1f36582fd6300ffd2d208d683adef6d0d8501423
Jayesh Bhoot <jayesh@bhoot.sh>
2022-12-19 22:48:03 +0530

    Simplify building pkgs

--- build-source-pkg-for-reviewers.sh
+++
@@ -1,3 +0,0 @@
-zip -r src-pkg.zip.tmp * -x '*.DS_Store*' '*_build*'
-rm -rf src-pkg.zip
-mv src-pkg.zip.tmp src-pkg.zip
--- build.sh
+++
@@ -1,3 +0,0 @@
-dune build --profile=release
-cd _build/default
-web-ext build --ignore-files="*.ml" "*.sh" "*.md" dune dune-project "*.zip" "*.bc"
